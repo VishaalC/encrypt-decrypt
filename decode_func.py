d32fc9fbe35f2568692b9b67262aa2b67fcaac90
AIJAY GOVARTHANAM <86974848+AIJAY1007X@users.noreply.github.com>
2021-07-06 20:25:52 +0530

    done it man

---
+++ decode_func.py
@@ -0,0 +1,8 @@
+def Decode(key,message):
+    dec=[]
+    message = base64.urlsafe_b64decode(message).decode()
+
+    for i in range(len(message)):
+        key_c = key[i % len(key)]
+        dec.append(chr((256 + ord(message[i])- ord(key_c)) % 256))
+    return "".join(dec)
